447310bb07e65deb9fdee9824daff8e6b8a17eef
Noah Haasis <haasis_noah@yahoo.de>
2019-03-15 17:10:14 +0100

    Add java and go solution

--- adidas-ascii-logo/go/main.go
+++ adidas-ascii-logo/go/main.go
@@ -2,9 +2,38 @@ package main
 
 import (
   "fmt"
+  "strings"
+  "math"
 )
+
+func replaceRange(s string, c rune, start, end int) string {
+  runes := []rune(s)
+  for i := start; i < end; i++ {
+    runes[i] = c;
+  }
+  return string(runes)
+}
+
 func createAdidasAsciiLogo(width int) string {
-  return fmt.Sprintf("%d",width)
+  firstRowHeight  := int(math.Round(math.Sqrt(float64(width))))
+  secondRowHeight := firstRowHeight * 2
+  logoHeight      := secondRowHeight + firstRowHeight
+
+  logo := make([]string, logoHeight)
+  // create first row
+  logo[0] = strings.Repeat(" ", width*2) + strings.Repeat("@", width)
+
+  for i := 1; i < logoHeight; i++ {
+    newRow := " " + logo[i-1]
+    if (logoHeight - firstRowHeight == i) { // start of first stripe
+      newRow = replaceRange(newRow, '@', 0, width)
+    } else if (logoHeight - secondRowHeight == i) { // start of second stripe
+      newRow = replaceRange(newRow, '@', width, width*2)
+    }
+    logo[i] = newRow
+  }
+
+  return strings.Join(logo, "\n")
 }
 
 func main(){
--- adidas-ascii-logo/java/src/main/java/com/adidas/AdidasAsciiLogo.java
+++ adidas-ascii-logo/java/src/main/java/com/adidas/AdidasAsciiLogo.java
@@ -1,9 +1,46 @@
 package com.adidas;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class AdidasAsciiLogo {
 
     public String createAdidasAsciiLogo(int width) {
-        return null;
+        if (width < 2) {
+            throw new IllegalArgumentException("Error, minimun width is 2");
+        }
+        
+        int firstStripeHeight = (int)Math.round(Math.sqrt(width));
+        int secondStripeHeight = firstStripeHeight*2;
+        int logoHeight = firstStripeHeight + secondStripeHeight;
 
+        ArrayList<String> logo = new ArrayList<String>(logoHeight);
+        // insert first row
+        logo.add(substituteRange(repeat(' ', width*3), '@', width*2, width*3));
+
+        for (int i = 1; i < logoHeight; i++) {
+            String newRow = " " + logo.get(i-1);
+            if (logoHeight - firstStripeHeight == i) { // start of first stripe
+                newRow = substituteRange(newRow, '@', 0, width);
+            } else if (logoHeight - secondStripeHeight == i) { // start of second stripe
+                newRow = substituteRange(newRow, '@', width, width*2);
+            }
+            logo.add(newRow);
+        }
+        return String.join("\n", logo) + "\n";
+    }
+
+    private String substituteRange(String src, char subs, int start, int end) {
+        char[] res = src.toCharArray();
+        for (int i = start; i < end; i++) {
+            res[i] = subs;
+        }
+        return new String(res);
+    }
+
+    private String repeat(char c, int count) {
+        char[] res = new char[count];
+        Arrays.fill(res, c);
+        return new String(res);
     }
 }
