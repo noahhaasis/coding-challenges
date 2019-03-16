eaad2eadcd0fccd17996904ca6b887e1ca4a92a4
Noah Haasis <haasis_noah@yahoo.de>
2019-03-16 06:56:59 +0100

    Solve the ascii-logo challenge in typescript

--- adidas-ascii-logo/typescript/src/adidas-ascii-logo.ts
+++ adidas-ascii-logo/typescript/src/adidas-ascii-logo.ts
@@ -4,5 +4,36 @@
  * @returns adidas logo.
  */
 export function createAdidasAsciiLogo(width: number): string {
-  return `${ width }`;
+  if (width < 2) {
+    throw new Error('Error, minimum width is 2');
+  }
+
+  const firstStripeHeight = Math.round(Math.sqrt(width));
+  const secondStripeHeight = firstStripeHeight * 2;
+  const logoHeight = firstStripeHeight + secondStripeHeight;
+
+  const stripePattern = '@'.repeat(width);
+  const logo: Array<String> = new Array(logoHeight);
+  // insert first row
+  logo[0] = ' '.repeat(width * 2) + stripePattern;
+
+  for (let i = 0; i < logo.length; i++) {
+    if (i === 0) continue;
+
+    let newRow = ` ${logo[i - 1]}`; // shift new row to the right
+    if (logoHeight - firstStripeHeight === i) {
+      // start first stripe
+      newRow = replaceRange(newRow, stripePattern, 0, width);
+    } else if (logoHeight - secondStripeHeight === i) {
+      // start second stripe
+      newRow = replaceRange(newRow, stripePattern, width, width * 2);
+    }
+    logo[i] = newRow;
+  }
+
+  return logo.join('\n');
+}
+
+function replaceRange(str: string, subs: string, start: number, end: number): string {
+  return `${str.substring(0, start)}${subs}${str.substring(end, str.length)}`;
 }
